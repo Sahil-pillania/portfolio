69893874325666553b1885394ccb62c75dd2204c
Sahil pillania <sahilpillania88@gmail.com>
2022-08-22 13:47:30 +0530

    Footer part

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./CSS/Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="footer">
+        <div className="tag">Copyright &copy; Sahil Pillania</div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
