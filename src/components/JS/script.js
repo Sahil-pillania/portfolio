a98ad223a6f670b166e8843c71ec7bbebb466cb9
Sahil pillania <sahilpillania88@gmail.com>
2022-08-27 21:36:57 +0530

    menu hide functionality

--- src/components/JS/script.js
+++ src/components/JS/script.js
@@ -3,18 +3,29 @@ window.onload = function () {
   var navBar = document.getElementById("nav");
   var button = document.getElementById("toggler");
   var black = document.getElementById("black");
+  var w = window.innerWidth;
+  console.log(w);
 
   // console.log(navBar);
-  // console.log(button);
+  // console.log(black);
 
   button.addEventListener("click", () => {
     navBar.classList.toggle("active");
     black.classList.toggle("active");
     //   alert("clicked");
   });
-};
 
-black.addEventListener("click", () => {
-  navBar.classList.toggle("active");
-  black.classList.toggle("active");
-});
+  black.addEventListener("click", () => {
+    navBar.classList.toggle("active");
+    black.classList.toggle("active");
+  });
+
+  window.addEventListener("resize", function (event) {
+    var newWidth = window.innerWidth;
+
+    if (newWidth > 720) {
+      navBar.classList.remove("active");
+      black.classList.remove("active");
+    }
+  });
+};
