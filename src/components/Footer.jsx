39e3f4a64540f1457405eb4df0cdad19a6ee124f
Sahil pillania <sahilpillania88@gmail.com>
2022-08-27 22:00:24 +0530

    added back button (navigation)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./CSS/Footer.css";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Button from "@mui/material/Button";
 
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="footer">
+        <div className="backButton">
+          <Button>
+            <ArrowBackIcon onClick={() => navigate(-1)} />
+          </Button>
+        </div>
         <div className="tag">Copyright &copy; Sahil Pillania</div>
       </div>
     </>
