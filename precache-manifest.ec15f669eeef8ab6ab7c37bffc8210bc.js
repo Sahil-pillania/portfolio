6d1beb46de195f2dc24d1ed0170de83c0c8d3b71
Sahil pillania <sahilpillania88@gmail.com>
2022-09-04 13:12:42 +0530

    Updates

---
+++ precache-manifest.ec15f669eeef8ab6ab7c37bffc8210bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "414ccb6d7b98dd7839f5",
+    "url": "/portfolio/static/css/main.1ed47219.chunk.css"
+  },
+  {
+    "revision": "414ccb6d7b98dd7839f5",
+    "url": "/portfolio/static/js/main.414ccb6d.chunk.js"
+  },
+  {
+    "revision": "9eb8d3dfd3c7c6f43da7",
+    "url": "/portfolio/static/js/1.9eb8d3df.chunk.js"
+  },
+  {
+    "revision": "a467f88e8e87e61ca0d5",
+    "url": "/portfolio/static/js/2.a467f88e.chunk.js"
+  },
+  {
+    "revision": "b7cb83e42917c1fab429",
+    "url": "/portfolio/static/js/runtime~main.b7cb83e4.js"
+  },
+  {
+    "revision": "f3fc375eb739e2fe40ff5908bf026d38",
+    "url": "/portfolio/index.html"
+  }
+];
